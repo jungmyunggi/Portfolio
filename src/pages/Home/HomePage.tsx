b53989668138eb51681f5dddaf348b3857502a19
정명기 <jmk101711@yu.ac.kr>
2025-03-06 20:27:01 +0900

    ✨ feat: Project 추가

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,6 +1,8 @@
 import Header from '@/features/Header/Header';
 import Nav from '@/features/Nav/Nav';
 import About from '@/features/About/About';
+import Skills from '@/features/Skills/Skills';
+import Projects from '@/features/Projects/Projects';
 export default function HomePage() {
   return (
     <>
@@ -8,6 +10,8 @@ export default function HomePage() {
       <div className="section">
         <Header />
         <About />
+        <Skills />
+        <Projects />
       </div>
     </>
   );
