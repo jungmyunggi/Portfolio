ac20fde0e0ddde9534a7070ede113c55f1664e80
정명기 <jmk101711@yu.ac.kr>
2025-03-08 21:34:29 +0900

    ✨ feat: Award추가

--- src/features/Awards/Awards.tsx
+++ src/features/Awards/Awards.tsx
@@ -1,3 +1,15 @@
-export default function Awards(){
-    return <p>Awards</p>
+import '@/features/Awards/Awards.scss';
+export default function Awards() {
+  return (
+    <section className="award">
+      <h2 className="award-title">수상 경력</h2>
+      <div className="award-content">
+        <div>
+          <p>한국정보통신학회 춘계 학술대회 우수논문</p>
+          <p>KIICE</p>
+        </div>
+        <p>프롬프트엔지니어링과 LLM을 활용한 IT인프라 비용 비교 플렛폼 논문 투고 및 수상</p>
+      </div>
+    </section>
+  );
 }
