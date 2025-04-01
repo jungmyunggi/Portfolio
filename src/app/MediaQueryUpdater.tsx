ff04dcad67fcbb3ee306a92fef952da7c4bf8f20
정명기 <jmk101711@yu.ac.kr>
2025-04-01 20:17:20 +0900

    💄 style: 반응형 범위 수정(1024px)

--- src/app/MediaQueryUpdater.tsx
+++ src/app/MediaQueryUpdater.tsx
@@ -2,12 +2,12 @@ import { useEffect } from 'react';
 import { useMediaQuery } from 'react-responsive';
 import { useMediaStore } from '@/app/store';
 export default function MediaQueryUpdater() {
-  const isMobileQuery = useMediaQuery({ maxWidth: 767 });
-  const setIsDesktop = useMediaStore((state) => state.setIsDesktop);
+    const isMobileQuery = useMediaQuery({ maxWidth: 1024 });
+    const setIsDesktop = useMediaStore((state) => state.setIsDesktop);
 
-  useEffect(() => {
-    setIsDesktop(!isMobileQuery);
-  }, [isMobileQuery, setIsDesktop]);
+    useEffect(() => {
+        setIsDesktop(!isMobileQuery);
+    }, [isMobileQuery, setIsDesktop]);
 
-  return null;
+    return null;
 }
