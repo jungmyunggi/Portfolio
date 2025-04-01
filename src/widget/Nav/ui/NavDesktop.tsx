f2a7fcabea63e6c8a085b19b356bd18768e17f05
정명기 <jmk101711@yu.ac.kr>
2025-04-01 22:52:00 +0900

    ✨ feat: Nav 로고 클릭시 Home으로 리다이렉트

--- src/widget/Nav/ui/NavDesktop.tsx
+++ src/widget/Nav/ui/NavDesktop.tsx
@@ -3,34 +3,37 @@ import SVGIcon from '@/share/ui/SVGIcon';
 import DarkModeToggle from '@/features/DarkModeToggle/ui/DarkModeToggle';
 import { useDarkStore } from '@/features/DarkModeToggle/model/DarkStore';
 export default function NavDesktop() {
-  const isDark = useDarkStore((state) => state.isDark);
-  return (
-    <nav className="navigation-desktop">
-      <SVGIcon
-        src="/assets/logo.svg"
-        height="150px"
-        width="150px"
-        shape={'square'}
-        fill={isDark === 'Dark' ? '#ffffff' : '#000000'}
-      />
-      <ul className="items">
-        <li className="item">
-          <Link to={'/'}>홈</Link>
-        </li>
-        <li className="item">
-          <Link to={'/introdution'}>자기소개</Link>
-        </li>
-        <li className="item">
-          <Link to={'/project'}>프로젝트</Link>
-        </li>
-        <li className="item">
-          <Link to={'/award&certification'}>자격증 및 수상</Link>
-        </li>
-        <li className="item">
-          <Link to={'/education'}>교육</Link>
-        </li>
-        <DarkModeToggle />
-      </ul>
-    </nav>
-  );
+    const isDark = useDarkStore((state) => state.isDark);
+    return (
+        <nav className="navigation-desktop">
+            <span>
+                <Link to={'/'} className="logo-link" />
+                <SVGIcon
+                    src="/assets/logo.svg"
+                    height="150px"
+                    width="150px"
+                    shape={'square'}
+                    fill={isDark === 'Dark' ? '#ffffff' : '#000000'}
+                />
+            </span>
+            <ul className="items">
+                <li className="item">
+                    <Link to={'/'}>홈</Link>
+                </li>
+                <li className="item">
+                    <Link to={'/introdution'}>자기소개</Link>
+                </li>
+                <li className="item">
+                    <Link to={'/project'}>프로젝트</Link>
+                </li>
+                <li className="item">
+                    <Link to={'/award&certification'}>자격증 및 수상</Link>
+                </li>
+                <li className="item">
+                    <Link to={'/education'}>교육</Link>
+                </li>
+                <DarkModeToggle />
+            </ul>
+        </nav>
+    );
 }
